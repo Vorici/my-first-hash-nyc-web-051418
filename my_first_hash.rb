43aee60686e598c5a27b317b1bd66edcece02400
Ledio Bode <l.bode515@gmail.com>
2018-05-14 00:57:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,14 +1,18 @@
 def my_hash
+  
 planets = {"mercury" => 1, "venus" => 2, "earth" => 3, "mars" => 4}
   
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-the_manifest = 
+  
+the_manifest = {
+  "whale bone corset" => 5,
+  "porcelain vase" => 2,
+  "oil painting" => 3
+}
+
 
 end
 
